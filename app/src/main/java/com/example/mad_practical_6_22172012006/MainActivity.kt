2b9b1fe768a67c02702dd49d0be75989bf2a2247
Student <student@gnu.ac.in>
2023-09-13 08:57:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_6_22172012006/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.mad_practical_6_22172012006
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var button:Button=findViewById(R.id.button2)
+        var button1:Button=findViewById(R.id.button4)
+        button.setOnClickListener{
+            playpause()
+        }
+        button1.setOnClickListener {
+            stop()
+        }
+    }
+    fun playpause()
+    {
+     Intent(applicationContext,MyService::class.java).putExtra(MyService.PLAYERKEY,MyService.PLAYERVALUE).apply {
+         startService(this)
+     }
+
+    }
+    fun stop()
+    {
+        Intent(applicationContext,MyService::class.java).putExtra(MyService.PLAYERKEY,MyService.PLAYERVALUE).apply {
+            stopService(this)
+        }
+
+    }
+}
